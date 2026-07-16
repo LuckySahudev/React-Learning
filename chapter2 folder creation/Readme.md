9fa01f6d79ebd01d5d2ad06223178b6dd889918c
LuckySahudev <sluckysahu18@gmail.com>
2026-07-16 13:37:11 +0530

    chapter 2  unit 1 folder Creation is complited

---
+++ chapter2 folder creation/Readme.md
@@ -0,0 +1,8 @@
+1. Creating a React folder using vite
+    command : for creation of react folder 
+        nmp create vite
+        -> that will create a react folder 
+
+    commad : for run the react app
+        nmp run dev
+    
---
+++ chapter2 folder creation/Unit 1 React first folder/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  return <>
+    <h1>Lucky sahu</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum fugiat nobis maxime porro, impedit, quidem ipsa doloribus praesentium, quaerat unde officiis officia ab reprehenderit. Porro quas ex obcaecati quos praesentium.</p>
+  </>
+  
+}
+
+export default App
